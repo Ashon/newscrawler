982f1a8244aed227b90e6b036191eb8830dfb548
ashon <ashon8813@gmail.com>
2018-12-14 03:04:45 +0900

    Add miscs

--- src/tests/test_mecab_ko.py
+++ src/tests/test_mecab_ko.py
@@ -5,6 +5,11 @@ m = mecab.MeCab()
 
 def test_mecab_should_be_working():
     text = '안녕하세요.'
-    parsed = m.pos(text)
 
-    assert type(parsed) is list
+    parsed_pos = m.pos(text)
+    parsed_nouns = m.nouns(text)
+    parsed_morphs = m.morphs(text)
+
+    assert type(parsed_pos) is list
+    assert type(parsed_nouns) is list
+    assert type(parsed_morphs) is list
