c27f567bf4bc7b9aa9bf8ad12d52c07273fbaa10
ashon <ashon8813@gmail.com>
2018-12-16 21:03:45 +0900

    Separate constants

--- src/crawlers.py
+++ src/crawlers.py
@@ -3,6 +3,22 @@ import re
 from core.crawler import CrawlingTarget
 
 
+FILTER_TEXT_PATTERNS = [
+    '\n',
+    '\t',
+    '▶.*',
+    'ⓒ.*',
+]
+
+FILTER_TAGS = [
+    'script',
+    'h4',
+    'span',
+    'a',
+    'strong'
+]
+
+
 class NaverNewsCrawlingTarget(CrawlingTarget):
     def get_content_wrapper(self, soup):
         content = soup.find_all(**self._selector)[0]
@@ -10,12 +26,13 @@ class NaverNewsCrawlingTarget(CrawlingTarget):
         return content
 
     def filter_content(self, content):
-        filter_tags = ['script', 'h4', 'span', 'a', 'strong']
-        for tag in filter_tags:
+        for tag in FILTER_TAGS:
             unwanted = content.find(tag)
             if unwanted:
                 unwanted.extract()
 
     def sanitize_content(self, content):
-        article_text = re.sub(r'[\n,\t]', '', content.text)
+        article_text = re.sub(
+            f'[{",".join(FILTER_TEXT_PATTERNS)}]', '',
+            content.text)
         return article_text
