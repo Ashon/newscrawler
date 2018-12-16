d1775a09e7f29d7acef4afa0ca0283a45e1f25b2
ashon <ashon8813@gmail.com>
2018-12-16 21:04:04 +0900

    WIP

--- src/main.py
+++ src/main.py
@@ -1,29 +1,46 @@
+from collections import defaultdict
+from datetime import datetime
+import itertools
+
 import mecab
 
-from crawler import CrawlingTarget
-from crawler import NaverNewsCrawlingTarget
+from core.crawler import CrawlingTarget
+from crawlers import NaverNewsCrawlingTarget
 
 import settings
 
 
-def main():
-    m = mecab.MeCab()
+MAX_PAGES_PER_DATE = 5
 
+
+def harvest(today):
     naver_news_list = CrawlingTarget(
         **settings.SPIDER_CONFIG['naver']['news_list'])
 
     naver_news = NaverNewsCrawlingTarget(
         **settings.SPIDER_CONFIG['naver']['news_page'])
 
-    news_links = naver_news_list.extract(date='20181211', page=1)
-    for link in news_links:
-        article_text = naver_news.extract(link=link['href'])
-        parsed = m.pos(article_text)
+    for i in range(MAX_PAGES_PER_DATE):
+        news_links = naver_news_list.extract(date=today, page=i)
+        for link in news_links:
+            article_text = naver_news.extract(link=link['href'])
+            yield article_text
 
-        print(link.text)
-        print(article_text)
-        print(parsed)
-        print('*' * 100)
+
+def main():
+    today = datetime.now().strftime('%Y%m%d')
+    iter_articles = harvest(today)
+
+    m = mecab.MeCab()
+    nouns = itertools.chain(*(m.nouns(article) for article in iter_articles))
+
+    bag_of_words = defaultdict(int)
+    for noun in nouns:
+        bag_of_words[noun] += 1
+    print(bag_of_words)
+
+    top_words = sorted(bag_of_words.items(), key=lambda x: x[1])
+    print(top_words[-20:])
 
 
 if __name__ == '__main__':
