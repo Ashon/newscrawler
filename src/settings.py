a30b37d0677488ec58fe6a3058c309e09fb58640
ashon <ashon8813@gmail.com>
2019-01-16 00:11:05 +0900

    Fix wrong task path

--- src/settings.py
+++ src/settings.py
@@ -49,9 +49,9 @@ SPIDER_CONFIG = {
 }
 
 TASK_ROUTES = {
+    'core.workflows.distribute_chain': {'queue': 'distribute_chain'},
     'worker.harvest_links': {'queue': 'harvest_links'},
     'worker.harvest_content': {'queue': 'harvest_content'},
-    'worker.distribute_chain': {'queue': 'distribute_chain'},
     'worker.extract_nouns': {'queue': 'extract_nouns'},
     'worker.aggregate_words': {'queue': 'aggregate_words'}
 }
