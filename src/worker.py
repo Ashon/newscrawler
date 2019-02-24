595f12feef74afc6420efff620aed9cd4141b1f5
ashon <ashon8813@gmail.com>
2019-02-24 17:37:57 +0900

    Fix lint error

--- src/worker.py
+++ src/worker.py
@@ -1,14 +1,13 @@
 from core.app import initialize_applictaion
+from core.tasks import DistributeChain
+from tasks.harvest import Harvest
+from tasks.extract import Extract
+from tasks.aggregate import Aggregate
 
 import settings
 
 
 app = initialize_applictaion(settings)
-
-from core.tasks import DistributeChain
-from tasks.harvest import Harvest
-from tasks.extract import Extract
-from tasks.aggregate import Aggregate
 
 app.register_task(DistributeChain())
 app.register_task(Harvest())
